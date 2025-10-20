aa0490b409879c0eeef49bc0e5834ed96122994f
Georgios Koutzos <koutzosgiorgos@gmail.com>
2025-10-20 19:40:49 +0300

    Adds model structure based on API endpoints

---
+++ models/models.go
@@ -0,0 +1,29 @@
+package models
+
+type Artist struct {
+	ID				int		 `json:"id"`
+	Image			string	 `json:"image"`
+	Name			string	 `json:"name"`
+	Members			[]string `json:"members"`
+	CreationDate	int		 `json:"creationDate"`
+	FirstAlbum		string	 `json:"firstAlbum"`
+	Locations		string	 `json:"locations"`
+	ConcertDates	string	 `json:"concertDates"`
+	Relations		string	 `json:"relations"`
+}
+
+type Locations struct {
+	ID				int		 `json:"id"`
+	Locations 		[]string `json:"locations"`
+	Dates			string	 `json:"dates"`
+}
+
+type Dates struct {
+	ID				int		 `json:"id"`
+	ConcertDates	[]string `json:"dates"`
+}
+
+type Relation struct {
+	ID				int		 			 `json:"id"`
+	DatesLocations	map[string][]string	 `json:"datesLocations"`
+}
