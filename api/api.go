df8605a3a2860ec92cc160939cf438ebc2d945a3
Georgios Koutzos <koutzosgiorgos@gmail.com>
2025-10-21 19:20:18 +0300

    Minor corrections

--- api/api.go
+++ api/api.go
@@ -3,13 +3,14 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"groupie-tracker/models"
 )
 
 func FetchArtists() []models.Artist {
 	resp, _ := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	defer resp.Body.Close()
 
-	artists := []models.Artist
+	var artists []models.Artist
 	json.NewDecoder(resp.Body).Decode(&artists)
 	return artists
 }
@@ -18,7 +19,7 @@ func FetchLocationsByURL(url string) []models.Locations {
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 
-	concert_locations := models.Locations
+	var concert_locations []models.Locations
 	json.NewDecoder(resp.Body).Decode(&concert_locations)
 	return concert_locations
 }
@@ -27,7 +28,7 @@ func FetchDatesByURL(url string) []models.Dates {
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 
-	concert_dates := models.Dates
+	var concert_dates []models.Dates
 	json.NewDecoder(resp.Body).Decode(&concert_dates)
 	return concert_dates
 }
@@ -36,7 +37,7 @@ func FetchRelationsByURL(url string) []models.Relation {
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 
-	relations := models.Relation
+	var relations []models.Relation
 	json.NewDecoder(resp.Body).Decode(&relations)
 	return relations
 }
