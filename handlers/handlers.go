89b6d76ceedbfc5864ae127c42cb8f8b09edba81
Georgios Koutzos <koutzosgiorgos@gmail.com>
2025-10-26 13:51:59 +0200

    Creates ArtistDetailsHandler()

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -39,6 +39,53 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func ArtistDetailsHandler(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, "artist/") {
+		HandleErrors(w, http.StatusNotFound, http.StatusText(http.StatusNotFound), "Please check the resource URL and try again.")
+		return
+	}
+	if r.Method != http.MethodGet {
+		HandleErrors(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed), "This request method is not supported for the requested resource. Use GET request instead.")
+		return
+	}
+	artist_ID, _ := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/artist/"))
+
+	artist, err := services.GetArtistByID(artist_ID)
+	if err != nil {
+		HandleErrors(w, http.StatusNotFound, http.StatusText(http.StatusNotFound), err.Error())
+		return
+	}
+	locations, err := services.GetLocationsByID(artist_ID)
+	if err != nil {
+		HandleErrors(w, http.StatusNotFound, http.StatusText(http.StatusNotFound), err.Error())
+		return
+	}
+	dates, err := services.GetDatesByID(artist_ID)
+	if err != nil {
+		HandleErrors(w, http.StatusNotFound, http.StatusText(http.StatusNotFound), err.Error())
+		return
+	}
+	relations, err := services.GetRelationsByID(artist_ID)
+	if err != nil {
+		HandleErrors(w, http.StatusNotFound, http.StatusText(http.StatusNotFound), err.Error())
+		return
+	}
+	data := struct {
+		ArtistDetails models.ArtistDetails
+	}{
+		ArtistDetails: models.ArtistDetails{
+			Artist:    *artist,
+			Locations: *locations,
+			Dates:     *dates,
+			Relations: *relations,
+		},
+	}
+	if err := artist_tmpl.Execute(w, data); err != nil {
+		HandleErrors(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError), "The server was unable to complete your request. Please try again later")
+		return
+	}
+}
+
 // HandleErrors renders an error page with the given status code, error message, and response.
 func HandleErrors(w http.ResponseWriter, statusCode int, message, response string) {
 	errorData := struct {
