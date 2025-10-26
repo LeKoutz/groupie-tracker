d6de85953d4c73a2488e4a915e4dd91870157743
Georgios Koutzos <koutzosgiorgos@gmail.com>
2025-10-26 13:51:19 +0200

    Creates HomeHandler()

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -19,6 +19,26 @@ var (
     error_tmpl  = template.Must(template.ParseFiles("templates/error.html"))
 )
 
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		HandleErrors(w, http.StatusNotFound, http.StatusText(http.StatusNotFound), "Please check the resource URL and try again.")
+		return
+	}
+	if r.Method != http.MethodGet {
+		HandleErrors(w, http.StatusMethodNotAllowed, http.StatusText(http.StatusMethodNotAllowed), "This request method is not supported for the requested resource. Use GET request instead.")
+		return
+	}
+	data := struct {
+		Artists []models.Artists
+	}{
+		Artists: api.All_Artists,
+	}
+	if err := index_tmpl.Execute(w, data); err != nil {
+		HandleErrors(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError), "The server was unable to complete your request. Please try again later")
+		return
+	}
+}
+
 // HandleErrors renders an error page with the given status code, error message, and response.
 func HandleErrors(w http.ResponseWriter, statusCode int, message, response string) {
 	errorData := struct {
