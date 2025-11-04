8a292e76a26aaf9a894270f03ccfe15b6ff68726
Georgios Koutzos <koutzosgiorgos@gmail.com>
2025-11-04 13:03:02 +0200

    Adds custom ServeMux

--- main.go
+++ main.go
@@ -16,9 +16,10 @@ func main () {
 	}
 	log.Printf("\nData loaded: %d artists, %d locations, %d dates, %d relations\nErrors: %v", len(api.All_Artists), len(api.All_Locations), len(api.All_Dates), len(api.All_Relations), err)
 	// Set up routes
-	http.HandleFunc("/", handlers.HomeHandler)
-	http.HandleFunc("/artist/", handlers.ArtistDetailsHandler)
-	http.HandleFunc("/static/", handlers.ResourcesHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.HomeHandler)
+	mux.HandleFunc("/artist/", handlers.ArtistDetailsHandler)
+	mux.HandleFunc("/static/", handlers.ResourcesHandler)
 
 	// Start the server
 	port := os.Getenv("PORT")
@@ -28,5 +29,5 @@ func main () {
 	addr := ":" + port
 	log.Println("Server starting on: http://localhost:" + port)
 	log.Println("Press CTRL+C to stop the server")
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
