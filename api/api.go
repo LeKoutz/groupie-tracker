00276186ef7c046f6de1878c472d0f812cafb687
Georgios Koutzos <koutzosgiorgos@gmail.com>
2025-11-06 08:00:25 +0000

    (#11) Create functions to fetch from API endpoints (#13)
    Resolves #11
    Squashed commits
    Reviewed-on: http://platform.zone01.gr/git/gkoutzos/groupie-tracker/pulls/13
    Reviewed-by: Constantine Ktistakis <cktistak@noreply.platform.zone01.gr>
    Reviewed-by: Tasos Tsopanidis <ttsopani@noreply.platform.zone01.gr>
    Co-authored-by: Georgios Koutzos <koutzosgiorgos@gmail.com>
    Co-committed-by: Georgios Koutzos <koutzosgiorgos@gmail.com>

---
+++ api/api.go
@@ -0,0 +1,118 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"groupie-tracker/models"
+	"net/http"
+)
+
+const (
+	ARTISTS_API		= "https://groupietrackers.herokuapp.com/api/artists"
+	LOCATIONS_API	= "https://groupietrackers.herokuapp.com/api/locations"
+	DATES_API		= "https://groupietrackers.herokuapp.com/api/dates"
+	RELATIONS_API	= "https://groupietrackers.herokuapp.com/api/relation"
+)
+
+var (
+	All_Artists		[]models.Artists
+	All_Locations	[]models.Locations
+	All_Dates		[]models.Dates
+	All_Relations	[]models.Relations
+)
+
+func InitializeData() []error {
+	var errors []error
+	var err error
+	All_Artists, err = FetchArtists()
+	if err != nil {
+		errors = append(errors, fmt.Errorf("FetchArtists: %v", err))
+	}
+	All_Locations, err = FetchLocations()
+	if err != nil {
+		errors = append(errors, fmt.Errorf("FetchLocations: %v", err))
+	}
+	All_Dates, err = FetchDates()
+	if err != nil {
+		errors = append(errors, fmt.Errorf("FetchDates: %v", err))
+	}
+	All_Relations, err = FetchRelations()
+	if err != nil {
+		errors = append(errors, fmt.Errorf("FetchRelations: %v", err))
+	}
+	if len(errors) > 0 {
+		return errors
+	} else {
+		return nil
+	}
+}
+
+func FetchArtists() ([]models.Artists, error) {
+	resp, err := http.Get(ARTISTS_API)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch from %s with error: %v", ARTISTS_API, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API Unexpected status: %d", resp.StatusCode)
+	}
+
+	var artists []models.Artists
+	if err := json.NewDecoder(resp.Body).Decode(&artists); err != nil {
+		return nil, fmt.Errorf("JSON decode failed: %v", err)
+	}
+	return artists, nil
+}
+
+func FetchLocations() ([]models.Locations, error) {
+	resp, err := http.Get(LOCATIONS_API)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch from %s with error: %v", LOCATIONS_API, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API Unexpected status: %d", resp.StatusCode)
+	}
+
+	var concert_locations models.LocationsIndex
+	if err := json.NewDecoder(resp.Body).Decode(&concert_locations); err != nil {
+		return nil, fmt.Errorf("JSON decode failed: %v", err)
+	}
+	return concert_locations.Index, nil
+}
+
+func FetchDates() ([]models.Dates, error) {
+	resp, err := http.Get(DATES_API)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch from %s with error: %v", DATES_API, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API Unexpected status: %d", resp.StatusCode)
+	}
+	var concert_dates models.DatesIndex
+	if err := json.NewDecoder(resp.Body).Decode(&concert_dates); err != nil {
+		return nil, fmt.Errorf("JSON decode failed: %v", err)
+	}
+	return concert_dates.Index, nil
+}
+
+func FetchRelations() ([]models.Relations, error) {
+	resp, err := http.Get(RELATIONS_API)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch from %s with error: %v", RELATIONS_API, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API Unexpected status: %d", resp.StatusCode)
+	}
+	var relations models.RelationIndex
+	if err := json.NewDecoder(resp.Body).Decode(&relations); err != nil {
+		return nil, fmt.Errorf("JSON decode failed: %v", err)
+	}
+	return relations.Index, nil
+}
