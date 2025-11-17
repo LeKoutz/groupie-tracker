6a6545bbf5c38eb0e8502ec684510370f57d5bb4
Georgios Koutzos <koutzosgiorgos@gmail.com>
2025-11-17 17:37:33 +0200

    InitializeData runs in a goroutine

--- main.go
+++ main.go
@@ -9,12 +9,16 @@ import (
 )
 
 func main () {
-	// Initialize the data structures
-	err := api.InitializeData()
-	if err != nil {
-		log.Printf("Failed to load data with error: %v", err)
-	}
-	log.Printf("\nData loaded: %d artists, %d locations, %d dates, %d relations\nErrors: %v", len(api.All_Artists), len(api.All_Locations), len(api.All_Dates), len(api.All_Relations), err)
+	go func() {
+		err := api.InitializeData()
+		if err != nil {
+			log.Printf("Failed to load data with error: %v", err)
+			api.SetLoadingStatus(false, false, true)
+		} else {
+			log.Printf("\nData loaded: %d artists, %d locations, %d dates, %d relations\nErrors: %v", len(api.All_Artists), len(api.All_Locations), len(api.All_Dates), len(api.All_Relations), err)
+			api.SetLoadingStatus(false, true, false)
+		}
+	}()
 	// Set up routes
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handlers.HomeHandler)
