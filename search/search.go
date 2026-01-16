21cc72a1cd3b6122a358fa23416386dee8982f8a
gkoutzos <koutzosgiorgos@gmail.com>
2026-01-16 21:47:33 +0200

    Allows for search by category selection

--- search/search.go
+++ search/search.go
@@ -10,6 +10,7 @@ import (
 type SearchResult struct {
 	Label string
 	ID    int
+	Category string
 }
 
 // SearchAll searches artists by name, members, first album, creation date, locations, and dates based on the query string
@@ -22,6 +23,7 @@ func SearchAll(query string, artists []models.Artists, getRelations func(int) (*
 			results = append(results, SearchResult{
 				Label: artist.Name + " - Artist/Band",
 				ID:    artist.ID,
+				Category: "artist",
 			})
 		}
 		// Search by members
@@ -30,6 +32,7 @@ func SearchAll(query string, artists []models.Artists, getRelations func(int) (*
 				results = append(results, SearchResult{
 					Label: member + " - Member of " + artist.Name,
 					ID:    artist.ID,
+					Category: "member",
 				})
 			}
 		}
@@ -38,6 +41,7 @@ func SearchAll(query string, artists []models.Artists, getRelations func(int) (*
 			results = append(results, SearchResult{
 				Label: artist.FirstAlbum + " - First Album of " + artist.Name,
 				ID:    artist.ID,
+				Category: "first_album",
 			})
 		}
 		// Search by creation date
@@ -46,6 +50,7 @@ func SearchAll(query string, artists []models.Artists, getRelations func(int) (*
 			results = append(results, SearchResult{
 				Label: creationDateStr + " - Creation Date of " + artist.Name,
 				ID:    artist.ID,
+				Category: "creation_date",
 			})
 		}
 		rel, err := getRelations(artist.ID)
@@ -61,12 +66,14 @@ func SearchAll(query string, artists []models.Artists, getRelations func(int) (*
 					results = append(results, SearchResult{
 						Label: date + " - Concert date at " + loc + " for " + artist.Name,
 						ID:    artist.ID,
+						Category: "concert",
 					})
 				}
 				if strings.Contains(strings.ToLower(loc), searchQuery) || strings.Contains(normalize(loc), normalize(query)) {
 					results = append(results, SearchResult{
 						Label: loc + " - Concert location on " + date + " for " + artist.Name,
 						ID:    artist.ID,
+						Category: "concert",
 					})
 				}
 			}
@@ -74,6 +81,7 @@ func SearchAll(query string, artists []models.Artists, getRelations func(int) (*
 	}
 	return results
 }
+
 // removes punctuation and spaces from string
 func normalize(s string) string {
 	s = strings.ToLower(s)
@@ -83,4 +91,17 @@ func normalize(s string) string {
 	s = strings.ReplaceAll(s, ".", "")
 	s = strings.ReplaceAll(s, "_", "")
 	return s
+}
+
+func FilterSearch(results []SearchResult, option string) []SearchResult {
+	if option == "all" {
+		return results
+	}
+	filtered := []SearchResult{}
+	for _, result := range results {
+		if result.Category == option {
+			filtered = append(filtered, result)
+		}
+	}
+	return filtered
 }
