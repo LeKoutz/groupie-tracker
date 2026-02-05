604fd9faed3b7f3bcae8409df0d7300cf3cba6a2
cktistak <ktistakis.c@gmail.com>
2026-02-05 19:47:33 +0200

    map data validation

---
+++ static/js/artist_map.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // check if map element and locations are present
+    const mapElement = document.getElementById("map");
+    const locations = window.artistMapData;
+    
+    if (!mapElement || !locations)
+        return;
+}
