8569d11a0fed3b0e553fe033fc4ab1cc9b988076
cktistak <ktistakis.c@gmail.com>
2026-02-07 17:44:30 +0200

    replaced counter with valid locations indexing

--- static/js/artist_map.js
+++ static/js/artist_map.js
@@ -6,30 +6,34 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!mapElement || !locations)
         return;
 
+  const validLocations = locations.filter(loc => {
+    const lat = parseFloat(loc.lat);
+    const lon = parseFloat(loc.lon);
+    return !isNaN(lat) && !isNaN(lon);
+  });
+
     // initialize the map
     const map = L.map(mapElement).setView([20, 0], 2); // center the map at (20, 0) with zoom level 2
 
     // add the tile layer
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: "&copy; OpenStreetMap &copy; Groupie Tracker",
-        subdomains: 'abcd', // provides multiple tile servers for better performance
+        subdomains: 'abc', // provides multiple tile servers for better performance
         maxZoom: 19, // maximum zoom level
     }).addTo(map);
 
     // create an array to store the bounds of the markers
   const bounds = [];
-  let markerCount = 0; // for valid markers
   
-    locations.forEach((location, index) => {
+    validLocations.forEach((location, index) => {
         const lat = parseFloat(location.lat);
         const lon = parseFloat(location.lon);
         // Filter out invalid coordinates
       if (!isNaN(lat) && !isNaN(lon)) {
-            markerCount++;
             const marker = L.marker([lat, lon]).addTo(map);
             // replace hyphens with spaces and convert to uppercase for the display name
             const displayName = location.name.replace(/[-]/g, ` `).toUpperCase();
-            marker.bindPopup(`<b>${markerCount}.${displayName}</b>`); // add the display name to the marker
+            marker.bindPopup(`<b>${index + 1}.${displayName}</b>`); // add the display name to the marker
 
             // Highlight path to next location when clicked
             marker.on('popupopen', () => {
@@ -37,8 +41,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (window.currentLine) map.removeLayer(window.currentLine);
 
                 // Is there a next location?
-                if (index < locations.length - 1) {
-                    const nextLoc = locations[index + 1];
+                if (index < validLocations.length - 1) {
+                    const nextLoc = validLocations[index + 1];
                     const nextLat = parseFloat(nextLoc.lat);
                     const nextLon = parseFloat(nextLoc.lon);
 
