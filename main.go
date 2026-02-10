7b46ff64b9288c0548d355932cda540576452da0
cktistak <ktistakis.c@gmail.com>
2026-02-10 22:50:04 +0200

    rm redundant go routine since it's already in one

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main () {
 func PrecacheAllLocations(relations []models.Relations) {
 	// map to ensure we geocode each location once
 	uniqueLocs := make(map[string]bool)
-	
+
 	for _, rel := range relations {
 		for locName := range rel.DatesLocations {
 			uniqueLocs[locName] = true
@@ -60,6 +60,6 @@ func PrecacheAllLocations(relations []models.Relations) {
 		allLocs = append(allLocs, locName)
 	}
 	log.Printf("Starting location geocoding for %d locations", len(allLocs))
-	go services.Geocode(allLocs)
+	services.Geocode(allLocs)
 	log.Println("Background geocoding complete")
-	}
+}
