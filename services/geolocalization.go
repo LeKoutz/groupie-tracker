f56e018d243fa54244689e212a6588066e9b9540
cktistak <ktistakis.c@gmail.com>
2026-02-11 19:09:38 +0200

    refactored geo-service + panic error fixup

--- services/geolocalization.go
+++ services/geolocalization.go
@@ -1,14 +1,14 @@
 package services
 
 import (
-	"sync"
-	"groupie-tracker/models"
-	"groupie-tracker/api"
-	"net/http"
 	"encoding/json"
+	"groupie-tracker/api"
+	"groupie-tracker/models"
+	"net/http"
 	"net/url"
-	"context"
+	"sync"
 	"time"
+	"context"
 )
 
 // memory cache
@@ -18,56 +18,49 @@ var (
 )
 
 // Geocode processes a list of locations and returns their coordinates.
-// It reuses the existing api.Client for timeouts and efficiency.
 func Geocode(locations []string) map[string]models.Coordinates {
 	results := make(map[string]models.Coordinates)
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-	 
-		for _, loc := range locations {
+
+	for _, loc := range locations {
 		// 1. Check Cache
 		geoMutex.RLock()
 		val, cached := geoCache[loc]
 		geoMutex.RUnlock()
+
 		if cached {
 			results[loc] = val
-			mu.Unlock()
 			continue
 		}
-		// fetch in parallel if not in cache
-		wg.Add(1)
-		go func(loc string) {
-			defer wg.Done()
-			
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
-			defer cancel()
 
 		// 2. Fetch from API
-		// We use url.QueryEscape to safely handle spaces and special chars
-		baseURL := "https://nominatim.openstreetmap.org/search?format=json&limit=1&q=" // This helper converts it to New+York or New%20York so the server understands it + we limit the results to the top one
+		// We sleep BEFORE the request to ensure we don't hit the limit.
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		
+		// Helper to safely handle spaces and special chars
+		baseURL := "https://nominatim.openstreetmap.org/search?format=json&limit=1&q="
 		req, err := http.NewRequestWithContext(ctx, "GET", baseURL+url.QueryEscape(loc), nil)
 		if err != nil {
-			return
+			cancel()
+			continue
 		}
-		req.Header.Set("User-Agent", "GroupieTracker") // set identity for api
+		 // Set identity for API (required by Novatim)
+		req.Header.Set("User-Agent", "GroupieTracker")
 		resp, err := api.Client.Do(req)
 		if err != nil {
-			return
+			cancel()
+			continue
 		}
-		defer resp.Body.Close()
-		
+
 		var data []models.Coordinates
-			// 3. Decode & Cache
-		if json.NewDecoder(resp.Body).Decode(&data) == nil && len(data) > 0 { // parse the response
+		// 3. Decode & Cache
+		if json.NewDecoder(resp.Body).Decode(&data) == nil && len(data) > 0 {
 			geoMutex.Lock()
 			geoCache[loc] = data[0] // store the best result in the cache
 			geoMutex.Unlock()
-			mu.Lock()
 			results[loc] = data[0]
-			mu.Unlock()
-			}
-		}(loc)
 		}
-	wg.Wait() // wait for all goroutines to finish or time out
+		resp.Body.Close()
+		cancel()
+	}
 	return results
 }
